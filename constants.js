22dfb8fd3408bac1f07ff5bef80f3a74ccfc5ccb
lnctchatbot <74965524+lnctchatbot@users.noreply.github.com>
2021-11-13 08:42:20 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,40 @@
+const prompts = [
+    ["college name","College name"],
+    ["college address","address","Address"],
+    ["email id","emailid","mail","email"],
+    ["tnp cell","t&p contact","tnp department","Tnp cell"],
+    ["Admission Cell","admission cell","Admission cell"],
+    ["Reception","reception"],
+    ["lnct hod","LNCT HOD","Lnct hod","lnct osd","Lnct osd","LNCT OSD","hod","Hod","HOD"]
+    
+  ]
+  
+  // Possible responses, in corresponding order
+  
+  const replies = [
+    ["Lakshmi Narain College of Technology"],
+    ["LNCT Campus, Kalchuri Nagar, (P.O. Kolua) Raisen Road, Bhopal-462022 (M.P)"],
+    ["info@lnct.ac.in"],
+    ["9 8 2 6 0 6 2 7 3 0, 0755-6185341"],
+    ["7440777111, 7201800001, 0755-6185350, 0755-6685400"],
+    [" : 0755-6185300,0755-6185300/1/2/3/4/5"],
+    ["Dr. Ashok Kumar Rai :618532,9827055261"]
+
+  
+   
+  ]
+  
+  // Random for any other user input
+  
+  const alternative = [
+    "Invalid",
+    "Information not present",
+    "Sorry",
+    "Try again",
+    "I don't understand :/"
+  ]
+  
+  // Whatever else you want :)
+  
+  const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
+  
